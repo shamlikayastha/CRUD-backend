4b68ce5a26610f78a0f9a89b344eb51a1a532831
shamlikayastha <2020bcs042@sggs.ac.in>
2024-01-06 00:31:04 +0530

    commit

---
+++ src/main/java/com/example/democrud/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.democrud.repository;
+
+import com.example.democrud.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
